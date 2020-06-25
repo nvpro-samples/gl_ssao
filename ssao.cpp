56bb2ca6ea1537dad471ed2d8adbc6603ca0560e
mklefrancois <38076163+mklefrancois@users.noreply.github.com>
2020-06-25 14:01:06 +0200

    Removing PROJECT_ABSDIRECTORY

--- ssao.cpp
+++ ssao.cpp
@@ -263,7 +263,6 @@ namespace ssao
     m_progManager.m_filetype = nvh::ShaderFileManager::FILETYPE_GLSL;
     m_progManager.addDirectory( std::string("GLSL_" PROJECT_NAME));
     m_progManager.addDirectory( exePath() + std::string(PROJECT_RELDIRECTORY));
-    //m_progManager.addDirectory( std::string(PROJECT_ABSDIRECTORY));
 
     m_progManager.registerInclude("common.h");
 
